9f80b48d09f1078c0b496c9fd138630112f4efa1
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-11-27 18:45:44 +0100

    Fixed

--- src/main/java/dev/naimsulejmani/grupi3fleetmanagement/controllers/DriversController.java
+++ src/main/java/dev/naimsulejmani/grupi3fleetmanagement/controllers/DriversController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -49,7 +50,8 @@ public class DriversController {
     }
 
     @PostMapping("/create")
-    public String addDriver() {
+    public String addDriver(@ModelAttribute Driver driver) {
+        service.add(driver);
         return "redirect:/drivers";
     }
 
